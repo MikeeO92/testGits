c6c04ed90a16cc14c1fbe6c40eea9189dad75c86
Meta Mike <metal-1415@hotmail.com>
2019-07-19 02:08:41 -0500

    Revert "se añade hola mundo"
    This reverts commit f228f39729bb58a7e237ff51dcc7f8fe4d466df6.

--- testGit/Program.cs
+++ testGit/Program.cs
@@ -10,11 +10,6 @@ namespace testGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
-            // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
         }
     }
 }
